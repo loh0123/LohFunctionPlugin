ac549d7b5c11f22420ffc4b34a3531c345f49cdf
loh zhi kang <loh0123@hotmail.com>
2023-03-09 20:49:11 +0800

    Optimize : Save System

--- Source/LohFunctionPlugin/Private/Voxel/LFPVoxelContainerComponent.cpp
+++ Source/LohFunctionPlugin/Private/Voxel/LFPVoxelContainerComponent.cpp
@@ -21,6 +21,7 @@ void ULFPVoxelContainerComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
+	Setting.InitSetting();
 }
 
 
@@ -29,32 +30,48 @@ void ULFPVoxelContainerComponent::TickComponent(float DeltaTime, ELevelTick Tick
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	if (UpdateChuckData())
+	/*if (UpdateChuckData())
 	{
 		UpdateChuckState();
-	}
+	}*/
 }
 
-FString ULFPVoxelContainerComponent::MemorySize() const
+FString ULFPVoxelContainerComponent::MemorySize(const int32 SaveFileAmount, const int32 ChuckAmount, const int32 VoxelAmount) const
 {
-	return FString::Printf(TEXT("%llu : ChuckDataList Size : %llu : Struct Size"), ChuckDataList.GetAllocatedSize(), sizeof(FColor));
+	unsigned long long TotalSize = (5 * VoxelAmount) * ChuckAmount * SaveFileAmount;
+	unsigned long long SaveSize = (5 * VoxelAmount) * ChuckAmount;
+	unsigned long long ChuckSize = (5 * VoxelAmount);
+
+	return FString::Printf(TEXT("Total : %llu : SaveData : %llu : ChuckData : %llu : Color : %llu"), 
+		TotalSize,
+		SaveSize,
+		ChuckSize,
+		sizeof(FColor));
 }
 
 /** Checker */
 
-bool ULFPVoxelContainerComponent::IsChuckInitialized(const FIntVector& ChuckVector) const
+bool ULFPVoxelContainerComponent::IsSaveInitialized(const int32 SaveIndex) const
 {
-	return ChuckDataList.Contains(ChuckVector);
+	return SaveDataList.IsValidIndex(SaveIndex) && SaveDataList[SaveIndex].ChuckData.IsEmpty() == false;
+}
+
+bool ULFPVoxelContainerComponent::IsChuckInitialized(const int32 SaveIndex, const int32 ChuckIndex) const
+{
+	return IsSaveInitialized(SaveIndex) && Setting.GetChuckLength() > ChuckIndex && SaveDataList[SaveIndex].ChuckData[ChuckIndex].IsInitialized();
 }
 
 bool ULFPVoxelContainerComponent::IsVoxelGridPositionValid(const FLFPVoxelGridPosition& VoxelGridPosition) const
 {
-	return IsChuckInitialized(VoxelGridPosition.ChuckVector) && Setting.GetVoxelLength() < VoxelGridPosition.VoxelIndex;
+	return Setting.GetVoxelLength() > VoxelGridPosition.VoxelIndex && Setting.GetChuckLength() > VoxelGridPosition.ChuckIndex && Setting.GetSaveLength() > VoxelGridPosition.SaveIndex;
 }
 
 bool ULFPVoxelContainerComponent::IsVoxelVisible(const FLFPVoxelGridPosition& VoxelGridPosition) const
 {
-	return IsVoxelGridPositionValid(VoxelGridPosition) && ChuckDataList.FindRef(VoxelGridPosition.ChuckVector).GetChuckMaterial(VoxelGridPosition.VoxelIndex) != 255;
+	return 
+		IsVoxelGridPositionValid(VoxelGridPosition) && 
+		IsChuckInitialized(VoxelGridPosition.SaveIndex, VoxelGridPosition.ChuckIndex) && 
+		SaveDataList[VoxelGridPosition.SaveIndex].ChuckData[VoxelGridPosition.ChuckIndex].GetChuckMaterial(VoxelGridPosition.VoxelIndex) != 255;
 }
 
 /** Setter */
@@ -63,7 +80,7 @@ bool ULFPVoxelContainerComponent::SetVoxelColor(const FLFPVoxelGridPosition& Vox
 {
 	if (IsVoxelGridPositionValid(VoxelGridPosition) == false) return false;
 
-	ChuckUpdateDataList.FindOrAdd(VoxelGridPosition.ChuckVector).ChangeColor.Add(VoxelGridPosition.VoxelIndex, VoxelColor);
+	ChuckUpdateDataList.FindOrAdd(VoxelGridPosition.GetSaveAndChuck()).ChangeColor.Add(VoxelGridPosition.VoxelIndex, VoxelColor);
 
 	return true;
 }
@@ -72,7 +89,7 @@ bool ULFPVoxelContainerComponent::SetVoxelMaterial(const FLFPVoxelGridPosition& 
 {
 	if (IsVoxelGridPositionValid(VoxelGridPosition) == false) return false;
 
-	ChuckUpdateDataList.FindOrAdd(VoxelGridPosition.ChuckVector).ChangeMaterial.Add(VoxelGridPosition.VoxelIndex, VoxelMaterial);
+	ChuckUpdateDataList.FindOrAdd(VoxelGridPosition.GetSaveAndChuck()).ChangeMaterial.Add(VoxelGridPosition.VoxelIndex, VoxelMaterial);
 
 	return true;
 }
@@ -81,135 +98,154 @@ bool ULFPVoxelContainerComponent::SetVoxelMaterial(const FLFPVoxelGridPosition& 
 
 FColor ULFPVoxelContainerComponent::GetVoxelColor(const FLFPVoxelGridPosition& VoxelGridPosition) const
 {
-	if (IsVoxelGridPositionValid(VoxelGridPosition) == false) return FColor(0);
+	if (IsVoxelGridPositionValid(VoxelGridPosition) == false || IsChuckInitialized(VoxelGridPosition.SaveIndex, VoxelGridPosition.ChuckIndex)) return FColor(0);
 
-	return ChuckDataList.FindChecked(VoxelGridPosition.ChuckVector).GetChuckColor(VoxelGridPosition.VoxelIndex);
+	return SaveDataList[VoxelGridPosition.SaveIndex].ChuckData[VoxelGridPosition.ChuckIndex].GetChuckColor(VoxelGridPosition.VoxelIndex);
 }
 
 uint8 ULFPVoxelContainerComponent::GetVoxelMaterial(const FLFPVoxelGridPosition& VoxelGridPosition) const
 {
-	if (IsVoxelGridPositionValid(VoxelGridPosition) == false) return uint8(255);
+	if (IsVoxelGridPositionValid(VoxelGridPosition) == false || IsChuckInitialized(VoxelGridPosition.SaveIndex, VoxelGridPosition.ChuckIndex)) return uint8(255);
 
-	return ChuckDataList.FindChecked(VoxelGridPosition.ChuckVector).GetChuckMaterial(VoxelGridPosition.VoxelIndex);
+	return SaveDataList[VoxelGridPosition.SaveIndex].ChuckData[VoxelGridPosition.ChuckIndex].GetChuckMaterial(VoxelGridPosition.VoxelIndex);
 }
 
-void ULFPVoxelContainerComponent::InitializeChuck_Implementation(const FIntVector& ChuckVector, const uint8 VoxelMaterial, const FColor VoxelColor)
+void ULFPVoxelContainerComponent::InitializeSave_Implementation(const int32 SaveIndex)
 {
-	FLFPVoxelChuckData& ChuckData = ChuckDataList.FindOrAdd(ChuckVector);
+	if (SaveDataList.IsValidIndex(SaveIndex) == false) return;
 
-	ChuckData.InitChuckData(Setting.GetVoxelLength(), VoxelMaterial, VoxelColor);
+	auto& ChuckData = SaveDataList[SaveIndex];
+
+	ChuckData.InitSaveData(Setting.GetChuckLength());
+
+	return;
+}
+
+void ULFPVoxelContainerComponent::InitializeChuck_Implementation(const int32 SaveIndex, const int32 ChuckIndex, const uint8 VoxelMaterial, const FColor VoxelColor)
+{
+	if (SaveDataList.IsValidIndex(SaveIndex) == false || SaveDataList[SaveIndex].ChuckData.IsValidIndex(ChuckIndex) == false) return;
+
+	auto& ChuckData = SaveDataList[SaveIndex].ChuckData[ChuckIndex];
+
+	ChuckData.InitChuckData(Setting.GetVoxelLength());
+
+	for (int32 VoxelIndex = 0; VoxelIndex < Setting.GetVoxelLength(); VoxelIndex++)
+	{
+		ChuckData.SetChuckColor(VoxelIndex, VoxelColor);
+		ChuckData.SetChuckMaterial(VoxelIndex, VoxelMaterial);
+	}
 
 	return;
 }
 
 /** Chuck Request */
 
-void ULFPVoxelContainerComponent::RequestRenderChuck(const FIntVector ChuckVector, FLFPVoxelChuckDelegate& ChuckDelegate)
-{
-	if (ChuckDelegateList.Contains(ChuckVector)) return;
-
-	ChuckDelegate = ChuckDelegateList.Add(ChuckVector);
-}
-
-void ULFPVoxelContainerComponent::ReleaseRenderChuck(const FIntVector ChuckVector)
-{
-	if (ChuckDelegateList.Contains(ChuckVector) == false) return;
-
-	ChuckDelegateList.FindChecked(ChuckVector).VoxelChuckUpdateEvent.Unbind();
-
-	ChuckDelegateList.Remove(ChuckVector);
-}
-
-void ULFPVoxelContainerComponent::RequestRenderData(const FIntVector ChuckVector, FLFPVoxelContainerSetting& ChuckSetting, TArray<FLFPVoxelChuckData*>& RenderData)
-{
-	checkf(ChuckDelegateList.Contains(ChuckVector) == false, TEXT("Chuck is not connect"));
-
-	ContainerThreadLock.ReadLock();
-
-	ChuckSetting = Setting;
-
-	RenderData.Empty(27);
-
-	for (int32 Z = -1; Z <= 1; Z++)
-	{
-		for (int32 Y = -1; Y <= 1; Y++)
-		{
-			for (int32 X = -1; X <= 1; X++)
-			{
-				RenderData.Add(ChuckDataList.Find(ChuckVector + FIntVector(X, Y, Z)));
-			}
-		}
-	}
-}
-
-void ULFPVoxelContainerComponent::ReleaseRenderData(const FIntVector ChuckVector)
-{
-	checkf(ChuckDelegateList.Contains(ChuckVector) == false, TEXT("Chuck is not connect"));
-
-	ContainerThreadLock.ReadUnlock();
-}
-
-void ULFPVoxelContainerComponent::UpdateChuckState()
-{
-	if (ChuckUpdateStateList.IsEmpty()) return;
-
-	auto ChuckUpdateState = ChuckUpdateStateList.CreateIterator();
-
-	const FLFPVoxelChuckDelegate* ChuckDelegate = ChuckDelegateList.Find(ChuckUpdateState.Key());
-
-	if (ChuckDelegate != nullptr) ChuckDelegate->VoxelChuckUpdateEvent.ExecuteIfBound(ChuckUpdateState.Value());
-
-	ChuckUpdateStateList.Remove(ChuckUpdateState.Key());
-
-	if (ChuckUpdateStateList.Num() == 0)
-	{
-		ChuckUpdateStateList.Shrink();
-	}
-
-	return;
-}
-
-bool ULFPVoxelContainerComponent::UpdateChuckData()
-{
-	if (ChuckUpdateDataList.IsEmpty()) return true;
-
-	if (ContainerThreadLock.TryWriteLock() == false) return false;
-
-	for (const auto& ChuckUpdate : ChuckUpdateDataList)
-	{
-		FLFPVoxelChuckData* ChuckData = ChuckDataList.Find(ChuckUpdate.Key);
-
-		if (ChuckData == nullptr) continue;
-
-		TSet<FIntVector> EdgeList = { FIntVector(0) };
-
-		/** This change the outdate voxel material */
-		for (const auto& ChangeVoxel : ChuckUpdate.Value.ChangeMaterial)
-		{
-			ChuckData->SetChuckMaterial(ChangeVoxel.Key, ChangeVoxel.Value);
-
-			EdgeList.Append(ULFPGridLibrary::GetGridEdgeDirection(ULFPGridLibrary::ToGridLocation(ChangeVoxel.Key, Setting.VoxelGridSize), Setting.VoxelGridSize));
-		}
-
-		/** This change the outdate voxel color */
-		for (const auto& ChangeVoxel : ChuckUpdate.Value.ChangeColor)
-		{
-			ChuckData->SetChuckColor(ChangeVoxel.Key, ChangeVoxel.Value);
-
-			EdgeList.Append(ULFPGridLibrary::GetGridEdgeDirection(ULFPGridLibrary::ToGridLocation(ChangeVoxel.Key, Setting.VoxelGridSize), Setting.VoxelGridSize));
-		}
-
-		/** This add update state to list */
-		for (const FIntVector& Edge : EdgeList)
-		{
-			FLFPVoxelChuckUpdateState& ChuckUpdateState = ChuckUpdateStateList.FindOrAdd(ChuckUpdate.Key + Edge);
-
-			if (ChuckUpdate.Value.ChangeColor.IsEmpty() == false) ChuckUpdateState.Color = true;
-			if (ChuckUpdate.Value.ChangeMaterial.IsEmpty() == false) ChuckUpdateState.Material = true;
-		}
-	}
-
-	ContainerThreadLock.WriteUnlock();
-
-	return true;
-}
+//void ULFPVoxelContainerComponent::RequestRenderChuck(const int32 SaveIndex, const int32 ChuckIndex, FLFPVoxelChuckDelegate& ChuckDelegate)
+//{
+//	if (ChuckDelegateList.Contains(ChuckVector)) return;
+//
+//	ChuckDelegate = ChuckDelegateList.Add(ChuckVector);
+//}
+//
+//void ULFPVoxelContainerComponent::ReleaseRenderChuck(const int32 SaveIndex, const int32 ChuckIndex)
+//{
+//	if (ChuckDelegateList.Contains(ChuckVector) == false) return;
+//
+//	ChuckDelegateList.FindChecked(ChuckVector).VoxelChuckUpdateEvent.Unbind();
+//
+//	ChuckDelegateList.Remove(ChuckVector);
+//}
+//
+//void ULFPVoxelContainerComponent::RequestRenderData(const int32 SaveIndex, const int32 ChuckIndex, FLFPVoxelContainerSetting& ChuckSetting, TArray<FLFPVoxelChuckData*>& RenderData)
+//{
+//	checkf(ChuckDelegateList.Contains(ChuckVector) == false, TEXT("Chuck is not connect"));
+//
+//	ContainerThreadLock.ReadLock();
+//
+//	ChuckSetting = Setting;
+//
+//	RenderData.Empty(27);
+//
+//	for (int32 Z = -1; Z <= 1; Z++)
+//	{
+//		for (int32 Y = -1; Y <= 1; Y++)
+//		{
+//			for (int32 X = -1; X <= 1; X++)
+//			{
+//				RenderData.Add(ChuckDataList.Find(ChuckVector + FIntVector(X, Y, Z)));
+//			}
+//		}
+//	}
+//}
+//
+//void ULFPVoxelContainerComponent::ReleaseRenderData(const int32 SaveIndex, const int32 ChuckIndex)
+//{
+//	checkf(ChuckDelegateList.Contains(ChuckVector) == false, TEXT("Chuck is not connect"));
+//
+//	ContainerThreadLock.ReadUnlock();
+//}
+//
+//void ULFPVoxelContainerComponent::UpdateChuckState()
+//{
+//	if (ChuckUpdateStateList.IsEmpty()) return;
+//
+//	auto ChuckUpdateState = ChuckUpdateStateList.CreateIterator();
+//
+//	const FLFPVoxelChuckDelegate* ChuckDelegate = ChuckDelegateList.Find(ChuckUpdateState.Key());
+//
+//	if (ChuckDelegate != nullptr) ChuckDelegate->VoxelChuckUpdateEvent.ExecuteIfBound(ChuckUpdateState.Value());
+//
+//	ChuckUpdateStateList.Remove(ChuckUpdateState.Key());
+//
+//	if (ChuckUpdateStateList.Num() == 0)
+//	{
+//		ChuckUpdateStateList.Shrink();
+//	}
+//
+//	return;
+//}
+//
+//bool ULFPVoxelContainerComponent::UpdateChuckData()
+//{
+//	if (ChuckUpdateDataList.IsEmpty()) return true;
+//
+//	if (ContainerThreadLock.TryWriteLock() == false) return false;
+//
+//	for (const auto& ChuckUpdate : ChuckUpdateDataList)
+//	{
+//		FLFPVoxelChuckData* ChuckData = ChuckDataList.Find(ChuckUpdate.Key);
+//
+//		if (ChuckData == nullptr) continue;
+//
+//		TSet<FIntVector> EdgeList = { FIntVector(0) };
+//
+//		/** This change the outdate voxel material */
+//		for (const auto& ChangeVoxel : ChuckUpdate.Value.ChangeMaterial)
+//		{
+//			ChuckData->SetChuckMaterial(ChangeVoxel.Key, ChangeVoxel.Value);
+//
+//			EdgeList.Append(ULFPGridLibrary::GetGridEdgeDirection(ULFPGridLibrary::ToGridLocation(ChangeVoxel.Key, Setting.VoxelGridSize), Setting.VoxelGridSize));
+//		}
+//
+//		/** This change the outdate voxel color */
+//		for (const auto& ChangeVoxel : ChuckUpdate.Value.ChangeColor)
+//		{
+//			ChuckData->SetChuckColor(ChangeVoxel.Key, ChangeVoxel.Value);
+//
+//			EdgeList.Append(ULFPGridLibrary::GetGridEdgeDirection(ULFPGridLibrary::ToGridLocation(ChangeVoxel.Key, Setting.VoxelGridSize), Setting.VoxelGridSize));
+//		}
+//
+//		/** This add update state to list */
+//		for (const FIntVector& Edge : EdgeList)
+//		{
+//			FLFPVoxelChuckUpdateState& ChuckUpdateState = ChuckUpdateStateList.FindOrAdd(ChuckUpdate.Key + Edge);
+//
+//			if (ChuckUpdate.Value.ChangeColor.IsEmpty() == false) ChuckUpdateState.Color = true;
+//			if (ChuckUpdate.Value.ChangeMaterial.IsEmpty() == false) ChuckUpdateState.Material = true;
+//		}
+//	}
+//
+//	ContainerThreadLock.WriteUnlock();
+//
+//	return true;
+//}
--- Source/LohFunctionPlugin/Public/Voxel/LFPVoxelContainerComponent.h
+++ Source/LohFunctionPlugin/Public/Voxel/LFPVoxelContainerComponent.h
@@ -16,14 +16,24 @@ public:
 
 	FLFPVoxelContainerSetting() {}
 
-	FLFPVoxelContainerSetting(FIntVector NewVoxelGridSize, FIntVector NewChuckGridSize) :
-		VoxelGridSize(NewVoxelGridSize) {}
+	FLFPVoxelContainerSetting(FIntVector NewVoxelGridSize, FIntVector NewChuckGridSize, FIntVector NewSaveGridSize) :
+		SaveGridSize(NewSaveGridSize), ChuckGridSize(NewChuckGridSize), VoxelGridSize(NewVoxelGridSize) {}
 
 private:
+
+	UPROPERTY() int32 SaveLength = 1;
+
+	UPROPERTY() int32 ChuckLength = 1;
 
 	UPROPERTY() int32 VoxelLength = 1;
 
 public:
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "VoxelContainerSetting | Setting")
+		FIntVector SaveGridSize = FIntVector(1);
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "VoxelContainerSetting | Setting")
+		FIntVector ChuckGridSize = FIntVector(1);
 
 	/* Size Of Voxel Inside Of A Chuck */
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "VoxelContainerSetting | Setting")
@@ -33,15 +43,30 @@ public:
 
 	FORCEINLINE void InitSetting()
 	{
-		if (VoxelGridSize.GetMin() <= 0.0 ) VoxelGridSize = FIntVector(1);
+		if (SaveGridSize.GetMin() <= 0) SaveGridSize = FIntVector(1);
+		if (ChuckGridSize.GetMin() <= 0) ChuckGridSize = FIntVector(1);
+		if (VoxelGridSize.GetMin() <= 0) VoxelGridSize = FIntVector(1);
 
+		SaveLength = SaveGridSize.X * SaveGridSize.Y * SaveGridSize.Z;
+		ChuckLength = ChuckGridSize.X * ChuckGridSize.Y * ChuckGridSize.Z;
 		VoxelLength = VoxelGridSize.X * VoxelGridSize.Y * VoxelGridSize.Z;
+	}
+
+	FORCEINLINE int32 GetSaveLength() const
+	{
+		return SaveLength;
+	}
+
+	FORCEINLINE int32 GetChuckLength() const
+	{
+		return ChuckLength;
 	}
 
 	FORCEINLINE int32 GetVoxelLength() const
 	{
 		return VoxelLength;
 	}
+
 };
 
 USTRUCT( BlueprintType )
@@ -51,17 +76,33 @@ struct FLFPVoxelGridPosition
 
 	FLFPVoxelGridPosition() {}
 
-	FLFPVoxelGridPosition(const FIntVector NewChuckVector, const int32 NewVoxelIndex) : ChuckVector(ChuckVector), VoxelIndex(NewVoxelIndex) {}
+	FLFPVoxelGridPosition(const int32 NewSaveIndex, const int32 NewChuckIndex, const int32 NewVoxelIndex) : SaveIndex(NewSaveIndex), ChuckIndex(NewChuckIndex), VoxelIndex(NewVoxelIndex) {}
 
 public:
 
+	/** Save Index In Array */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LFPVoxelGridPosition")
+		int32 SaveIndex = INDEX_NONE;
+
 	/** Chuck Index In Array */
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LFPVoxelGridPosition")
-		FIntVector ChuckVector = FIntVector::NoneValue;
+		int32 ChuckIndex = INDEX_NONE;
 
 	/** Voxel Index In Array */
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LFPVoxelGridPosition")
 		int32 VoxelIndex = INDEX_NONE;
+
+public:
+
+	FORCEINLINE FIntPoint GetSaveAndChuck() const
+	{
+		return FIntPoint(SaveIndex, ChuckIndex);
+	}
+
+	FORCEINLINE bool operator==(const FLFPVoxelGridPosition& Other) const
+	{
+		return SaveIndex == Other.SaveIndex && ChuckIndex == Other.ChuckIndex && VoxelIndex == Other.VoxelIndex;
+	}
 };
 
 
@@ -73,50 +114,78 @@ struct FLFPVoxelChuckData
 private:
 
 	UPROPERTY(SaveGame)
-		TArray<uint8> VoxelMaterial = {};
-
-	UPROPERTY(SaveGame) 
-		TArray<FColor> VoxelColor = {};
+		TArray<uint8> VoxelBtyeList = {};
 
 public:
 
 	FORCEINLINE bool IsInitialized() const
 	{
-		return VoxelMaterial.IsEmpty() == false;
+		return VoxelBtyeList.IsEmpty() == false;
 	}
 
-	FORCEINLINE void InitChuckData(const int32 VoxelLength, const uint8 NewVoxelMaterial, const FColor& NewVoxelColor)
+	FORCEINLINE void InitChuckData(const int32 VoxelLength)
 	{
-		VoxelMaterial.Init(NewVoxelMaterial, VoxelLength);
-		VoxelColor.Init(NewVoxelColor, VoxelLength);
+		const int32 BtyeLength = 5;
+
+		VoxelBtyeList.Init(uint8(0), VoxelLength * BtyeLength);
 	}
 
 	FORCEINLINE void SetChuckMaterial(const int32 VoxelIndex, const uint8 NewVoxelMaterial)
 	{
-		check(VoxelMaterial.IsValidIndex(VoxelIndex));
+		const int32 BtyeLength = 5;
 
-		VoxelMaterial[VoxelIndex] = NewVoxelMaterial;
+		check(VoxelBtyeList.IsValidIndex(VoxelIndex * BtyeLength));
+
+		VoxelBtyeList[VoxelIndex * BtyeLength] = NewVoxelMaterial;
 	}
 
 	FORCEINLINE void SetChuckColor(const int32 VoxelIndex, const FColor& NewVoxelColor)
 	{
-		check(VoxelColor.IsValidIndex(VoxelIndex));
+		const int32 BtyeLength = 5;
 
-		VoxelColor[VoxelIndex] = NewVoxelColor;
+		check(VoxelBtyeList.IsValidIndex(VoxelIndex * BtyeLength));
+
+		VoxelBtyeList[VoxelIndex * BtyeLength + 1] = NewVoxelColor.B;
+		VoxelBtyeList[VoxelIndex * BtyeLength + 2] = NewVoxelColor.G;
+		VoxelBtyeList[VoxelIndex * BtyeLength + 3] = NewVoxelColor.R;
+		VoxelBtyeList[VoxelIndex * BtyeLength + 4] = NewVoxelColor.A;
 	}
 
 	FORCEINLINE const uint8 GetChuckMaterial(const int32 VoxelIndex) const
 	{
-		check(VoxelMaterial.IsValidIndex(VoxelIndex));
+		const int32 BtyeLength = 5;
 
-		return VoxelMaterial[VoxelIndex];
+		check(VoxelBtyeList.IsValidIndex(VoxelIndex * BtyeLength));
+
+		return VoxelBtyeList[VoxelIndex * BtyeLength];
 	}
 
-	FORCEINLINE const FColor& GetChuckColor(const int32 VoxelIndex) const
+	FORCEINLINE FColor GetChuckColor(const int32 VoxelIndex) const
 	{
-		check(VoxelColor.IsValidIndex(VoxelIndex));
+		const int32 BtyeLength = 5;
 
-		return VoxelColor[VoxelIndex];
+		check(VoxelBtyeList.IsValidIndex(VoxelIndex * BtyeLength));
+
+		return FColor(VoxelBtyeList[VoxelIndex * BtyeLength + 3], VoxelBtyeList[VoxelIndex * BtyeLength + 2], VoxelBtyeList[VoxelIndex * BtyeLength + 1], VoxelBtyeList[VoxelIndex * BtyeLength + 4]);
+	}
+
+};
+
+USTRUCT()
+struct FLFPVoxelSaveData
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(SaveGame)
+		TArray<FLFPVoxelChuckData> ChuckData = {};
+
+public:
+
+	FORCEINLINE void InitSaveData(const int32 ChuckLength)
+	{
+		ChuckData.Init(FLFPVoxelChuckData(), ChuckLength);
 	}
 
 };
@@ -136,7 +205,7 @@ struct FLFPVoxelChuckUpdateState
 
 public: // Operator
 
-	FLFPVoxelChuckUpdateState& operator+=(const FLFPVoxelChuckUpdateState& Other)
+	FORCEINLINE FLFPVoxelChuckUpdateState& operator+=(const FLFPVoxelChuckUpdateState& Other)
 	{
 		if (Other.Color) Color = true;
 		if (Other.Material) Material = true;
@@ -205,17 +274,20 @@ public:
 public: /** Debugging */
 
 	UFUNCTION(BlueprintCallable, Category = "LFPVoxelContainerComponent | Function")
-		FORCEINLINE FString MemorySize() const;
+		FORCEINLINE FString MemorySize(const int32 SaveFileAmount, const int32 ChuckAmount, const int32 VoxelAmount) const;
 
 public: /** Checker */
 
-	UFUNCTION(BlueprintPure, Category = "LFPVoxelContainerComponent | Function")
-		FORCEINLINE bool IsChuckInitialized(const FIntVector& ChuckVector) const;
+	UFUNCTION(BlueprintPure, Category = "LFPVoxelContainerComponent | Checker")
+		FORCEINLINE bool IsSaveInitialized(const int32 SaveIndex) const;
 
-	UFUNCTION(BlueprintPure, Category = "LFPVoxelContainerComponent | Function")
+	UFUNCTION(BlueprintPure, Category = "LFPVoxelContainerComponent | Checker")
+		FORCEINLINE bool IsChuckInitialized(const int32 SaveIndex, const int32 ChuckIndex) const;
+
+	UFUNCTION(BlueprintPure, Category = "LFPVoxelContainerComponent | Checker")
 		FORCEINLINE bool IsVoxelGridPositionValid(const FLFPVoxelGridPosition& VoxelGridPosition) const;
 
-	UFUNCTION(BlueprintPure, Category = "LFPVoxelContainerComponent | Function")
+	UFUNCTION(BlueprintPure, Category = "LFPVoxelContainerComponent | Checker")
 		FORCEINLINE bool IsVoxelVisible(const FLFPVoxelGridPosition& VoxelGridPosition) const;
 
 public: /** Setter */
@@ -237,28 +309,32 @@ public: /** Getter */
 public:
 
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "LFPVoxelContainerComponent | Chuck")
-		FORCEINLINE void InitializeChuck(const FIntVector& ChuckVector, const uint8 VoxelMaterial, const FColor VoxelColor);
-		virtual void InitializeChuck_Implementation(const FIntVector& ChuckVector, const uint8 VoxelMaterial, const FColor VoxelColor);
+		FORCEINLINE void InitializeSave(const int32 SaveIndex);
+		virtual void InitializeSave_Implementation(const int32 SaveIndex);
 
-public: /** Chuck Request */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "LFPVoxelContainerComponent | Chuck")
+		FORCEINLINE void InitializeChuck(const int32 SaveIndex, const int32 ChuckIndex, const uint8 VoxelMaterial, const FColor VoxelColor);
+		virtual void InitializeChuck_Implementation(const int32 SaveIndex, const int32 ChuckIndex, const uint8 VoxelMaterial, const FColor VoxelColor);
 
-	/** Request chuck info on chuck spawn */
-	FORCEINLINE void RequestRenderChuck(const FIntVector ChuckVector, FLFPVoxelChuckDelegate& ChuckDelegate);
-
-	/** Release chuck info on chuck destroy */
-	FORCEINLINE void ReleaseRenderChuck(const FIntVector ChuckVector);
-
-	/** Request Render Data and lock write access */
-	FORCEINLINE void RequestRenderData(const FIntVector ChuckVector, FLFPVoxelContainerSetting& ChuckSetting, TArray<FLFPVoxelChuckData*>& RenderData);
-
-	/** Release write access */
-	FORCEINLINE void ReleaseRenderData(const FIntVector ChuckVector);
-
-protected: /** Function for updating chuck and data */
-
-	FORCEINLINE void UpdateChuckState();
-
-	FORCEINLINE bool UpdateChuckData();
+//public: /** Chuck Request */
+//
+//	/** Request chuck info on chuck spawn */
+//	FORCEINLINE void RequestRenderChuck(const int32 SaveIndex, const int32 ChuckIndex, FLFPVoxelChuckDelegate& ChuckDelegate);
+//
+//	/** Release chuck info on chuck destroy */
+//	FORCEINLINE void ReleaseRenderChuck(const int32 SaveIndex, const int32 ChuckIndex);
+//
+//	/** Request Render Data and lock write access */
+//	FORCEINLINE void RequestRenderData(const int32 SaveIndex, const int32 ChuckIndex, FLFPVoxelContainerSetting& ChuckSetting);
+//
+//	/** Release write access */
+//	FORCEINLINE void ReleaseRenderData(const int32 SaveIndex, const int32 ChuckIndex);
+//
+//protected: /** Function for updating chuck and data */
+//
+//	FORCEINLINE void UpdateChuckState();
+//
+//	FORCEINLINE bool UpdateChuckData();
 
 public: /** Read and write lock */
 
@@ -274,17 +350,17 @@ protected:  // Runtime Data
 
 	/** This store the chuck */
 	UPROPERTY(SaveGame) 
-		TMap<FIntVector, FLFPVoxelChuckData> ChuckDataList;
+		TArray<FLFPVoxelSaveData> SaveDataList;
 
 	/** This store chuck delegate to call */
 	UPROPERTY() 
-		TMap<FIntVector, FLFPVoxelChuckDelegate> ChuckDelegateList;
+		TMap<FIntPoint, FLFPVoxelChuckDelegate> ChuckDelegateList;
 
 	/** This store future chuck update data to apply */
 	UPROPERTY() 
-		TMap<FIntVector, FLFPVoxelUpdateAction> ChuckUpdateDataList;
+		TMap<FIntPoint, FLFPVoxelUpdateAction> ChuckUpdateDataList;
 
 	/** This store future chuck event data to send */
 	UPROPERTY()
-		TMap<FIntVector, FLFPVoxelChuckUpdateState> ChuckUpdateStateList;
+		TMap<FIntPoint, FLFPVoxelChuckUpdateState> ChuckUpdateStateList;
 };
