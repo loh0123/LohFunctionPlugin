eff1f7dbfa69826c6e4521d33e1f4d424caa5045
loh zhi kang <loh0123@hotmail.com>
2023-06-11 15:59:20 +0800

    - Fix Typo

--- Source/LohFunctionPlugin/Public/Item/LFPItemMutatorComponent.h
+++ Source/LohFunctionPlugin/Public/Item/LFPItemMutatorComponent.h
@@ -46,7 +46,7 @@ DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnProcessMutatorItemEvent, const F
 */
 
 UCLASS( Blueprintable, ClassGroup = (LFPlugin), meta = (BlueprintSpawnableComponent) )
-class CHRONOFORGE_API ULFPItemMutatorComponent : public UActorComponent
+class LOHFUNCTIONPLUGIN_API ULFPItemMutatorComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
