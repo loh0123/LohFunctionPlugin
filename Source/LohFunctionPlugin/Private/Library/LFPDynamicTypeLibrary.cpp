4cc0cba4ffebe52ae80b1b2619f6609574ce7990
loh0123 <loh0123@hotmail.com>
2024-12-21 19:00:23 +0800

    Add Tag Ref List

--- Source/LohFunctionPlugin/Private/Library/LFPDynamicTypeLibrary.cpp
+++ Source/LohFunctionPlugin/Private/Library/LFPDynamicTypeLibrary.cpp
@@ -1,3 +1,4 @@
 #include "Library/LFPDynamicTypeLibrary.h"
 
 DEFINE_LOG_CATEGORY( LFPDynamicIntStaticArray );
+
--- Source/LohFunctionPlugin/Public/Library/LFPDynamicTypeLibrary.h
+++ Source/LohFunctionPlugin/Public/Library/LFPDynamicTypeLibrary.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "LFPDynamicTypeLibrary.generated.h"
 
@@ -17,7 +18,7 @@ struct LOHFUNCTIONPLUGIN_API FLFPDynamicIntStaticArray
 	FLFPDynamicIntStaticArray( const uint32 NewIndexSize ) : IndexSize( NewIndexSize ) {
 	}
 
-	FLFPDynamicIntStaticArray( const uint32 NewIndexSize , const uint32 NewMinSize ) : IndexSize( NewIndexSize ) , MinElementBitSize( NewMinSize ) {
+	FLFPDynamicIntStaticArray( const uint32 NewIndexSize , const uint8 NewDataAlignment ) : IndexSize( NewIndexSize ) , ElementAlignment( NewDataAlignment ) {
 	}
 
 private:
@@ -32,7 +33,7 @@ private:
 	uint8 ElementBitSize = 0;
 
 	UPROPERTY()
-	uint8 MinElementBitSize = 0;
+	uint8 ElementAlignment = 4;
 
 private:
 
@@ -62,7 +63,7 @@ private:
 
 		if ( NewSize > 0 )
 		{
-			NewSize = FMath::Max( NewSize , MinElementBitSize );
+			NewSize = FMath::DivideAndRoundUp( NewSize , ElementAlignment ) * ElementAlignment;
 		}
 
 		if ( NewSize == ElementBitSize ) return;
@@ -164,7 +165,7 @@ public:
 		return OutIndex;
 	}
 
-	/** Debug Function */
+public:
 
 	FORCEINLINE uint8 GetEncodeLength() const
 	{
@@ -176,6 +177,253 @@ public:
 		return IndexSize;
 	}
 
+};
+
+USTRUCT( BlueprintType )
+struct LOHFUNCTIONPLUGIN_API FLFPIDTrackerStaticArray : public FLFPDynamicIntStaticArray
+{
+	GENERATED_BODY()
+
+	FLFPIDTrackerStaticArray() {
+	}
+
+	FLFPIDTrackerStaticArray( const uint32 NewIndexSize ) : Super( NewIndexSize ) {
+	}
+
+	FLFPIDTrackerStaticArray( const uint32 NewIndexSize , const uint8 NewDataAlignment ) : Super( NewIndexSize , NewDataAlignment ) {
+	}
+
+private:
+
+	UPROPERTY()
+	TArray<int32> OpenIDList = {};
+
+	UPROPERTY()
+	TArray<int32> IDRefList = {};
+
+protected:
+
+	/** Resize Function */
+
+	FORCEINLINE void ResizeID()
+	{
+		for ( int32 IDIndex = IDRefList.Num() - 1; IDIndex >= 0; --IDIndex )
+		{
+			if ( IDRefList[ IDIndex ] == 0 )
+			{
+				IDRefList.RemoveAt( IDIndex , EAllowShrinking::No );
+
+				OpenIDList.HeapRemoveAt( OpenIDList.IndexOfByKey( IDIndex ) , EAllowShrinking::No );
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		OpenIDList.Shrink();
+		IDRefList.Shrink();
+
+		ResizeElement( IDRefList.Num() );
+
+		return;
+	}
+
+public:
+
+	FORCEINLINE int32 GetNewID() const
+	{
+		if ( OpenIDList.IsEmpty() == false )
+		{
+			return OpenIDList[ 0 ];
+		}
+		else
+		{
+			return IDRefList.Num();
+		};
+	}
+
+	FORCEINLINE void RemoveRef( const int32 IDIndex )
+	{
+		check( IDRefList.IsValidIndex( IDIndex ) );
+
+		check( IDRefList[ IDIndex ] > 0 );
+
+		IDRefList[ IDIndex ]--;
+
+		if ( IDRefList[ IDIndex ] == 0 )
+		{
+			if ( IDRefList.Num() - 1 == IDIndex )
+			{
+				IDRefList.RemoveAt( IDIndex );
+
+				ResizeID();
+			}
+			else
+			{
+				OpenIDList.HeapPush( IDIndex );
+			}
+		}
+	}
+
+	FORCEINLINE void RemoveRefOnArrayIndex( const int32 ArrayIndex )
+	{
+		check( IsValidIndex( ArrayIndex ) );
+
+		RemoveRef( GetID( ArrayIndex ) );
+	}
+
+	FORCEINLINE void AddRef( const int32 IDIndex )
+	{
+		bool bNeedResize = false;
+
+		if ( IDRefList.IsValidIndex( IDIndex ) == false )
+		{
+			const int32 OldNum = IDRefList.Num();
+
+			IDRefList.SetNum( IDIndex + 1 );
+
+			for ( int32 Index = OldNum; Index < IDRefList.Num() - 1; Index++ )
+			{
+				OpenIDList.HeapPush( Index );
+			}
+
+			bNeedResize = true;
+		}
+		else if ( IDRefList[ IDIndex ] == 0 )
+		{
+			// In case this ID come from OpenIDList
+			if ( const int32 OpenID = OpenIDList.IndexOfByKey( IDIndex ); OpenID != INDEX_NONE )
+			{
+				OpenIDList.HeapRemoveAt( OpenID );
+			}
+		}
+
+		check( IDRefList[ IDIndex ] >= 0 );
+
+		IDRefList[ IDIndex ]++;
+
+		if ( bNeedResize )
+		{
+			ResizeID();
+		}
+	}
+
+	FORCEINLINE int32 GetID( const int32 ArrayIndex ) const
+	{
+		check( IsValidIndex( ArrayIndex ) );
+
+		return GetIndexNumber( ArrayIndex ) - 1;
+	}
+
+	FORCEINLINE void SetID( const int32 ArrayIndex , const int32 IDIndex )
+	{
+		check( IsValidIndex( ArrayIndex ) );
+		check( IDIndex + 1 >= 0 );
+
+		const int32 IndexCurrentID = GetID( ArrayIndex );
+
+		if ( IndexCurrentID == IDIndex )
+		{
+			return;
+		}
+
+		if ( IndexCurrentID > 0 )
+		{
+			RemoveRefOnArrayIndex( ArrayIndex );
+		}
+
+		if ( IDIndex >= 0 )
+		{
+			AddRef( IDIndex );
+		}
+
+		SetIndexNumber( ArrayIndex , IDIndex + 1 );
+
+		return;
+	}
+
+	FORCEINLINE int32 IDLength() const
+	{
+		if ( HasData() == false )
+		{
+			return 0;
+		}
+
+		return IDRefList.Num();
+	}
+};
+
+USTRUCT( BlueprintType )
+struct LOHFUNCTIONPLUGIN_API FLFPRefTagStaticArray : public FLFPIDTrackerStaticArray
+{
+	GENERATED_BODY()
+
+	FLFPRefTagStaticArray() {
+	}
+
+	FLFPRefTagStaticArray( const uint32 NewIndexSize ) : Super( NewIndexSize ) {
+	}
+
+	FLFPRefTagStaticArray( const uint32 NewIndexSize , const uint8 NewDataAlignment , const FGameplayTag& StartTag ) : Super( NewIndexSize , NewDataAlignment )
+	{
+		if ( StartTag.IsValid() == false )
+		{
+			return;
+		}
+
+		ItemList.Add( StartTag );
+
+		for ( uint32 ArrayIndex = 0; ArrayIndex < NewIndexSize; ArrayIndex++ )
+		{
+			SetID( ArrayIndex , 0 );
+		}
+	}
+
+private:
+
+	UPROPERTY()
+	TArray<FGameplayTag> ItemList = {};
+
+public:
+
+	FORCEINLINE void SetItem( const int32 ArrayIndex , const FGameplayTag& Item )
+	{
+		check( IsValidIndex( ArrayIndex ) );
+
+		int32 NewItemIndex = INDEX_NONE;
+
+		if ( Item.IsValid() )
+		{
+			if ( NewItemIndex = ItemList.IndexOfByKey( Item ); NewItemIndex != INDEX_NONE )
+			{
+				NewItemIndex = GetNewID();
+
+				if ( ItemList.IsValidIndex( NewItemIndex ) == false )
+				{
+					ItemList.SetNum( NewItemIndex + 1 );
+				}
+
+				ItemList[ NewItemIndex ] = Item;
+			}
+		}
+
+		SetID( ArrayIndex , NewItemIndex );
+
+		ItemList.SetNum( IDLength() );
+	}
+
+	FORCEINLINE const TArray<FGameplayTag>& GetItemList() const
+	{
+		return ItemList;
+	}
+
+	FORCEINLINE FGameplayTag GetItem( const int32 ArrayIndex ) const
+	{
+		const int32 ID = GetID( ArrayIndex );
+
+		return ID != INDEX_NONE ? ItemList[ ID ] : FGameplayTag::EmptyTag;
+	}
 };
 
 UCLASS()
