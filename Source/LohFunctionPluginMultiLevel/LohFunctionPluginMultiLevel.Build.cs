ec09ae97213a7998f18dc53f011062c4754a8395
loh0123 <loh0123@hotmail.com>
2025-05-23 00:21:43 +0800

    Commit LFP Multi Level Module

---
+++ Source/LohFunctionPluginMultiLevel/LohFunctionPluginMultiLevel.Build.cs
@@ -0,0 +1,27 @@
+﻿using UnrealBuildTool;
+
+public class LohFunctionPluginMultiLevel : ModuleRules
+{
+	public LohFunctionPluginMultiLevel(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"Engine"
+			}
+		);
+
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore"
+			}
+		);
+	}
+}
