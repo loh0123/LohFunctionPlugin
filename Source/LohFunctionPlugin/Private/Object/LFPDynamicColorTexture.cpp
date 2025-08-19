bb19221a5d7a8cf3ad21f3b8c8e2720980c9f7ea
loh0123 <loh0123@hotmail.com>
2025-08-19 16:02:12 +0800

    - Add Dynamic Color Texture Object

---
+++ Source/LohFunctionPlugin/Private/Object/LFPDynamicColorTexture.cpp
@@ -0,0 +1,70 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Object/LFPDynamicColorTexture.h"
+
+UTexture2D* ULFPDynamicColorTexture::GetTexture ( ) const
+{
+	return ColorTexture;
+}
+
+void ULFPDynamicColorTexture::Init ( const FIntPoint Size , const TextureFilter Filter , const bool bSRGB )
+{
+	if ( IsValid ( ColorTexture ) )
+	{
+		ColorTexture->MarkAsGarbage ( );
+		ColorTexture = nullptr;
+	}
+
+	ColorTexture = UTexture2D::CreateTransient ( Size.X , Size.Y );
+	ColorList.SetNum ( Size.X * Size.Y * 4 );
+
+#if WITH_EDITORONLY_DATA
+	ColorTexture->MipGenSettings = TMGS_NoMipmaps;
+#endif
+	ColorTexture->CompressionSettings = TC_VectorDisplacementmap;
+	ColorTexture->SRGB                = bSRGB;
+	ColorTexture->Filter              = Filter;
+	ColorTexture->UpdateResource ( );
+
+	TextureRegion = MakeUnique < FUpdateTextureRegion2D > ( 0 , 0 , 0 , 0 , Size.X , Size.Y );
+	Upload ( );
+}
+
+void ULFPDynamicColorTexture::SetPixelColor ( const int32 X , const int32 Y , const FColor& NewColor )
+{
+	if ( IsValid ( ColorTexture ) == false )
+	{
+		return;
+	}
+
+	const int32 ListIndex = ( X + Y * ColorTexture->GetSizeX ( ) ) * 4;
+
+	ColorList [ ListIndex ]     = NewColor.B;
+	ColorList [ ListIndex + 1 ] = NewColor.G;
+	ColorList [ ListIndex + 2 ] = NewColor.R;
+	ColorList [ ListIndex + 3 ] = NewColor.A;
+}
+
+void ULFPDynamicColorTexture::Upload ( )
+{
+	if ( IsValid ( ColorTexture ) == false )
+	{
+		return;
+	}
+
+	constexpr int32 BufferSize = 4;
+
+#if WITH_EDITOR
+	ColorTexture->TemporarilyDisableStreaming ( );
+#endif
+
+	ColorTexture->UpdateTextureRegions (
+	                                    0 ,
+	                                    1 ,
+	                                    TextureRegion.Get ( ) ,
+	                                    ColorTexture->GetSizeX ( ) * BufferSize ,
+	                                    BufferSize ,
+	                                    ColorList.GetData ( )
+	                                   );
+}
