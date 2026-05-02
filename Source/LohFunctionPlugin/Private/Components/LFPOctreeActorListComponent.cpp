10eb7193569234ca4179bf73ba5a91e924255446
loh0123 <loh0123@hotmail.com>
2026-05-03 01:27:27 +0800

    - Added Octree Actor List Component

---
+++ Source/LohFunctionPlugin/Private/Components/LFPOctreeActorListComponent.cpp
@@ -0,0 +1,90 @@
+﻿// Copyright by LOH ZHI KANG
+
+
+#include "Components/LFPOctreeActorListComponent.h"
+
+
+FLFPOctreeActorOctree::FLFPOctreeActorOctree ( )
+{
+}
+
+FLFPOctreeActorOctree::FLFPOctreeActorOctree ( const FVector& Origin , FVector::FReal Radius ) : TOctree2 < FLFPOctreeActorElement , FLFPOctreeActorSemantics > ( Origin , Radius )
+{
+}
+
+FLFPOctreeActorOctree::~FLFPOctreeActorOctree ( )
+{
+}
+
+void FLFPOctreeActorOctree::AddNode ( const FBoxCenterAndExtent& Bounds , AActor* Actor , const FLFPOctreeActorOctreeIDSharedRef& ID )
+{
+	AddElement ( FLFPOctreeActorElement ( Actor , Bounds , ID ) );
+}
+
+void FLFPOctreeActorOctree::UpdateNode ( const FOctreeElementId2& Id , const FBox& NewBounds )
+{
+	FLFPOctreeActorElement ElementCopy = GetElementById ( Id );
+	RemoveElement ( Id );
+	ElementCopy.Bounds = NewBounds;
+	AddElement ( ElementCopy );
+}
+
+void FLFPOctreeActorOctree::RemoveNode ( const FOctreeElementId2& Id )
+{
+	RemoveElement ( Id );
+}
+
+// Sets default values for this component's properties
+ULFPOctreeActorListComponent::ULFPOctreeActorListComponent ( )
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+
+	// ...
+}
+
+
+// Called when the game starts
+void ULFPOctreeActorListComponent::BeginPlay ( )
+{
+	Super::BeginPlay ( );
+
+	UpdateOctreeBounds ( );
+}
+
+
+// Called every frame
+void ULFPOctreeActorListComponent::TickComponent ( float DeltaTime , ELevelTick TickType , FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent ( DeltaTime , TickType , ThisTickFunction );
+
+	// ...
+}
+
+void ULFPOctreeActorListComponent::Add ( AActor* Actor , const FBox& OctreeBounds , const FLFPOctreeActorOctreeHandle& OutHandle )
+{
+	ActorOctree.AddNode ( OctreeBounds , Actor , OutHandle.SharedOctreeID );
+}
+
+void ULFPOctreeActorListComponent::Remove ( const FLFPOctreeActorOctreeHandle& Handle )
+{
+	ActorOctree.RemoveNode ( Handle.SharedOctreeID.Get ( ).ID );
+}
+
+void ULFPOctreeActorListComponent::Find ( const FBox& QueryBox , TArray < AActor* >& OutResults ) const
+{
+	ActorOctree.FindElementsWithBoundsTest ( QueryBox ,
+	                                         [&OutResults] ( const FLFPOctreeActorElement& Element )
+	                                         {
+		                                         if ( Element.Value.IsValid ( ) )
+		                                         {
+			                                         OutResults.Add ( Element.Value.Get ( ) );
+		                                         }
+	                                         } );
+}
+
+void ULFPOctreeActorListComponent::UpdateOctreeBounds ( )
+{
+	ActorOctree = FLFPOctreeActorOctree ( GetComponentLocation ( ) , Bounds.BoxExtent.Size ( ) );
+}
---
+++ Source/LohFunctionPlugin/Public/Components/LFPOctreeActorListComponent.h
@@ -0,0 +1,124 @@
+﻿// Copyright by LOH ZHI KANG
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/BoxComponent.h"
+#include "LFPOctreeActorListComponent.generated.h"
+
+using FLFPOctreeActorOctreeIDSharedRef = TSharedRef < struct FLFPOctreeActorOctreeID , ESPMode::ThreadSafe >;
+
+struct FLFPOctreeActorOctreeID : public TSharedFromThis < FLFPOctreeActorOctreeID , ESPMode::ThreadSafe >
+{
+	FOctreeElementId2 ID;
+};
+
+struct FLFPOctreeActorElement
+{
+	FLFPOctreeActorElement ( ) = default;
+
+	explicit FLFPOctreeActorElement ( AActor* InValue , const FBoxCenterAndExtent& InBounds , const FLFPOctreeActorOctreeIDSharedRef& InID ) : ID ( InID )
+	                                                                                                                                           , Value ( InValue )
+	                                                                                                                                           , Bounds ( InBounds )
+	{
+	}
+
+	FLFPOctreeActorOctreeIDSharedRef ID     = FLFPOctreeActorOctreeIDSharedRef ( );
+	TWeakObjectPtr < AActor >        Value  = nullptr;
+	FBoxCenterAndExtent              Bounds = { };
+};
+
+struct FLFPOctreeActorSemantics
+{
+	enum { MaxElementsPerLeaf = 16 };
+
+	enum { MinInclusiveElementsPerNode = 7 };
+
+	enum { MaxNodeDepth = 12 };
+
+	using ElementAllocator = TInlineAllocator < MaxElementsPerLeaf >;
+
+	inline static const FBoxCenterAndExtent& GetBoundingBox ( const FLFPOctreeActorElement& Element )
+	{
+		return Element.Bounds;
+	}
+
+	inline static bool AreElementsEqual ( const FLFPOctreeActorElement& A , const FLFPOctreeActorElement& B )
+	{
+		return A.Value == B.Value;
+	}
+
+	static void SetElementId ( const FLFPOctreeActorElement& Element , FOctreeElementId2 OctreeElementID )
+	{
+		Element.ID.Get ( ).ID = OctreeElementID;
+	}
+};
+
+struct FLFPOctreeActorOctree : TOctree2 < FLFPOctreeActorElement , FLFPOctreeActorSemantics >
+{
+public:
+
+	FLFPOctreeActorOctree ( );
+	FLFPOctreeActorOctree ( const FVector& Origin , FVector::FReal Radius );
+	virtual ~FLFPOctreeActorOctree ( );
+
+	/** Add new node and initialize using SmartObject runtime data */
+	void AddNode ( const FBoxCenterAndExtent& Bounds , AActor* Actor , const FLFPOctreeActorOctreeIDSharedRef& ID );
+
+	/** Updates element bounds remove/add operation */
+	void UpdateNode ( const FOctreeElementId2& Id , const FBox& NewBounds );
+
+	/** Remove node */
+	void RemoveNode ( const FOctreeElementId2& Id );
+};
+
+USTRUCT ( BlueprintType )
+struct FLFPOctreeActorOctreeHandle
+{
+	GENERATED_BODY ( )
+
+	FLFPOctreeActorOctreeHandle ( ) : SharedOctreeID ( MakeShareable ( new FLFPOctreeActorOctreeID ( ) ) )
+	{
+	}
+
+	FLFPOctreeActorOctreeIDSharedRef SharedOctreeID;
+};
+
+UCLASS ( ClassGroup=(Custom) , meta=(BlueprintSpawnableComponent) )
+class LOHFUNCTIONPLUGIN_API ULFPOctreeActorListComponent : public UBoxComponent
+{
+	GENERATED_BODY ( )
+
+public:
+
+	// Sets default values for this component's properties
+	ULFPOctreeActorListComponent ( );
+
+protected:
+
+	// Called when the game starts
+	virtual void BeginPlay ( ) override;
+
+public:
+
+	// Called every frame
+	virtual void TickComponent ( float DeltaTime , ELevelTick TickType , FActorComponentTickFunction* ThisTickFunction ) override;
+
+protected:
+
+	UFUNCTION ( BlueprintCallable , Category=Default )
+	void Add ( AActor* Actor , const FBox& OctreeBounds , const FLFPOctreeActorOctreeHandle& OutHandle );
+
+	UFUNCTION ( BlueprintCallable , Category=Default )
+	void Remove ( const FLFPOctreeActorOctreeHandle& Handle );
+
+	UFUNCTION ( BlueprintCallable , Category=Default )
+	void Find ( const FBox& QueryBox , TArray < AActor* >& OutResults ) const;
+
+	//UFUNCTION ( BlueprintCallable , Category=Default )
+	void UpdateOctreeBounds ( );
+
+private:
+
+	FLFPOctreeActorOctree ActorOctree;
+};
